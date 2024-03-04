fa0f26dfe6f0f363b1b55ccfa939fa882e25d2aa
Mauricio Pinzon <mauricio.pinzon@8base.com>
2024-03-04 11:51:56 -0500

    Version3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import "dotenv/config";
 import logo from "./logo.svg";
 import "./App.css";
 
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { FileInput as NewFileInput } from "@8base/file-manager";
 
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql, useMutation } from "@apollo/client";
 
-const FILMS_QUERY = gql`
+const FILE_LIST_QUERY = gql`
   query filesList {
     filesList {
       count
@@ -21,12 +21,100 @@ const FILMS_QUERY = gql`
   }
 `;
 
+const MY_MUTATION = gql`
+  mutation Table_s3Create($input: Table_s3CreateInput!) {
+    table_s3Create(data: $input) {
+      id
+      name
+      file_image_01 {
+        id
+        fileId
+        filename
+        downloadUrl
+        shareUrl
+        meta
+        public
+      }
+      file_image_02 {
+        id
+        fileId
+        filename
+        downloadUrl
+        shareUrl
+        meta
+        public
+      }
+      file_file_01 {
+        id
+        fileId
+        filename
+        downloadUrl
+        shareUrl
+        meta
+        public
+      }
+      file_file_02 {
+        id
+        fileId
+        filename
+        downloadUrl
+        shareUrl
+        meta
+        public
+      }
+    }
+  }
+`;
+
 function App() {
-  const { data, loading, error } = useQuery(FILMS_QUERY);
+  const [createRow, { dataCreate }] = useMutation(MY_MUTATION);
+  const handleCreateRow = () => {
+    createRow({
+      variables: {
+        input: {
+          name: "TestFromReactApp",
+          file_image_01: {
+            create: {
+              fileId: fileId,
+              filename: nameFile,
+              public: false,
+            },
+          },
+        },
+      },
+    });
+  };
+
+  const { data, loading, error } = useQuery(FILE_LIST_QUERY);
 
   const handleClickModal = (openModal) => {
     console.log("clicked");
     openModal();
+  };
+
+  const handleClickPick = useCallback(async (pick) => {
+    console.log("clicked test");
+    console.log(pick);
+    const accept = "img"
+    const picker = await pick({
+      onFileUploadFailed: async (file, error) => {
+        if (error && error.message === "UPLOAD_CANCELLED_ERROR") {
+          return;
+        }
+        //const errorMessage = getFilestackErrorMessage(file, error);
+        await picker.close();
+        //toast.error(errorMessage);
+      },
+      ...(accept ? { accept } : {}),
+    });
+    }, []);
+
+  const [nameFile, setnameFile] = useState("Name file..");
+  const [fileId, setfileId] = useState(null);
+
+  const handleGetResponse = (file) => {
+    setnameFile(file[0].filename);
+    setfileId(file[0].fileId);
   };
 
   if (loading)
@@ -49,44 +137,54 @@ function App() {
       </div>
     );
 
+  if (dataCreate) console.log("dataCreate: " + dataCreate);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>File manager</h1>
 
+        <label>Name:</label>
+        <br></br>
+        <input type="text" placeholder="Name.."></input>
+        <br></br>
         <NewFileInput
           apiKey={process.env.REACT_APP_API_TOKEN}
-          onChange={(file) => console.log(file)}
-          useFilestack={true}
+          onChange={handleGetResponse}
+          //onChange={(file) => console.log("Data: " + file[0].fileId)}
+          useFilestack={false}
           maxFiles={2}
           value={null}
           workspace={process.env.REACT_APP_WORKSPACE}
           environment={process.env.REACT_APP_ENV}
           uploadHost={`https://qa4-file-manager.8basedev.com`}
         >
-          {({ openModal }) => (
+          {({ pick }) => (
             <>
               <button
                 className="App-button"
-                onClick={() => handleClickModal(openModal)}
+                onClick={() => handleClickModal(pick)}
               >
-                Button to Load
+                Button Text
               </button>
             </>
           )}
         </NewFileInput>
+        <br></br>
+        <input type="text" value={nameFile} readOnly></input>
+        <br></br>
+        <button className="App-button" onClick={handleCreateRow}>
+          Add Row
+        </button>
+        <br></br>
 
         <img src={logo} className="App-logo" alt="logo" />
         <h3>filesList - count: {data.filesList.count}</h3>
         <ul className="App-ul">
           {data.filesList.items.map((file, index) => (
             <>
-              <li key={index + "id"}>
-                {file.fileId}
-              </li>
-              <li key={index + "name"}>
-                {file.filename}
-              </li>
+              <li key={index + "id"}>{file.fileId}</li>
+              <li key={index + "name"}>{file.filename}</li>
             </>
           ))}
         </ul>
